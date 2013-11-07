ad6c7ed9b85fdb7497c2b365545d9f24a04c37b3
Mike Taylor <mtaylor@mica.edu>
2013-11-07 07:01:51 -0500

    trip counter finished

--- directions_scrape/mapquest_directions.py
+++ directions_scrape/mapquest_directions.py
@@ -1,29 +1,38 @@
+#expects station1, lat1, long1, station2, lat2, long2
+#add url success check before processing...
+#write each line sequentially instead of waiting until end
 import os, json
 from urllib import urlopen, quote
+from time import sleep
 
 API_KEY = 'Fmjtd%7Cluubnuu7lu%2C8s%3Do5-9uya9u'
 URL = 'http://open.mapquestapi.com/directions/v1/route?key=%s&outFormat=json&routeType=bicycle&generalize=5&from=%s&to=%s'
 geojson = '{"type": "FeatureCollection","features": [%s]}'
 features = []
-newFeature = '{ "type": "Feature", "id": %s, "properties": { "from": %s, "to": %s }, "geometry": { "type": "LineString", "coordinates": %s } }'
+newFeature = '{ "type": "Feature", "id": %s, "properties": { "from": %s, "to": %s, "count": %s}, "geometry": { "type": "LineString", "coordinates": %s } }'
 id = 0
-with open('cabi_stations_2012_test.csv', 'r') as fr:
+with open('cabi_trips_2012.csv', 'r') as fr:
 	lines = fr.readlines()
 	lines = [l.strip() for l in lines]
 	routes =  [l.split(',') for l in lines]
 
 	for r in routes:
 		s = ','.join(r[1:3])
-		d = ','.join(r[4:])
+		d = ','.join(r[4:6])
 		if (s == d) or (r[0] == 'sTERMINAL_N'):
 			continue
 		else:
+			print id
 			data = urlopen(URL %(API_KEY, s, d)).read()
 			directions = json.loads(data)
 			shape = directions['route']['shape']['shapePoints']
 			formattedShape = [[shape[i], shape[i-1]] for i, x in enumerate(shape) if (i%2 != 0)]
-			features.append(newFeature %(id, r[0], r[3], formattedShape))
+			features.append(newFeature %(id, r[0], r[3], r[6], formattedShape))
+			
 			id+=1
+			
+		sleep(1)
+
 with open('cabi_routes.geojson', 'w') as fw:
 	fw.write(geojson %(','.join(features)))
 
--- trip_counter/trip_counter.py
+++ trip_counter/trip_counter.py
@@ -1,5 +1,30 @@
 #Step 1 - iterate through file and pull out station pairs, increment counter for each pair
-with open ('sample_trip_data.csv') as f:
-	for line in f:
-		trip = [l.split(',') for l in line]
-		print trip
+#Read in station coords and store
+#Create a dictionary that with the station pair as key and the count as the value
+#As a new station pair is found, write the combo to a new file structured in the format needed for the direction download
+trips = {}
+stations = {}
+with open ('2012-combined.csv', 'r') as rawTrips, open('cabi_stations.csv', 'r') as stationCoords, open('cabi_trips_2012.csv', 'w') as tripOut:
+	for station in stationCoords:
+		s = station.strip().split(',')
+		stations[s[0]] = s[1],s[2]
+	for line in rawTrips:
+		if line.find('Start')!=-1:
+			continue
+		l = line.split(',')
+		station_pair = l[4], l[7]
+		#check to see if we have coords for station
+		if station_pair[0] not in stations or station_pair[1] not in stations:
+			continue
+			
+		#basic counter of trip amounts
+		if station_pair in trips:
+			trips[station_pair][6] += 1
+		else:
+			trips[station_pair] = [station_pair[0], stations[station_pair[0]][0], stations[station_pair[0]][1], station_pair[1], stations[station_pair[1]][0], stations[station_pair[1]][1], 1]
+
+		#format station1, lat1, long1, station2, lat2, long2, count
+	for t in trips:
+		trips[t][6] = str(trips[t][6])
+		tripOut.write ('%s' %(','.join(trips[t])))
+		tripOut.write ('\n')
