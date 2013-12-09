a845036276cd2acb9c4e534b71649de3cc346d10
Mike Taylor <mtaylor@mica.edu>
2013-12-08 22:27:37 -0500

    more fiddling

--- web/js/map.js
+++ web/js/map.js
@@ -2,7 +2,8 @@
 //add time rings around each station
 //animate the bar graph and route drawing
 //make bar graph interact with map
-var stationSelected = 0, 
+var stationSelected = 0,
+  station2Selected = 0,
   w_graph = window.innerWidth/3.25,
   h_graph = window.innerHeight/1.7,
   stationLUT = {};
@@ -19,6 +20,8 @@ var map = new L.Map("map", {
   //zoomControl: false,
   maxZoom: 14, 
   minZoom: 12,
+  attribution: 'Directions courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a>',
+  attribution: '2012 ride data provided by <a href="http://www.capitalbikeshare.com/" target="_blank">Capital Bikeshare</a> '
 })
 
 .addLayer( new L.TileLayer("http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png", {opacity: 0.2}));
@@ -56,31 +59,30 @@ d3.json("data/cabi_stations_2012.geojson", function(collection) {
       var coordinates = d3.mouse(this);
       //checks to see if user click is on a new origin node or a shift+click to select an end
       if (d3.event.shiftKey) {
+        station2Selected = d.properties.TERMINAL_N;
         d3.select(this).attr("class", "stationNode sn_end_selected").style("fill-opacity", "1");
         d3.selectAll(".sn_end").style("fill-opacity", ".25");
-        d3.selectAll(".sn_unselected").style("fill-opacity", ".25");
-        
+        d3.selectAll(".sn_unselected").style("fill-opacity", ".25"); 
         showRoutes(stationSelected, d.properties.TERMINAL_N);    
       }
       else {
         gText.selectAll("text").remove();
         graph.selectAll("rect").data([]).exit().remove();
         stationSelected = d.properties.TERMINAL_N;
+        station2Selected = 0;
         d3.selectAll(".stationNode").attr("class", "stationNode sn_unselected").style("fill","#B6B6B6").style("fill-opacity", "1").transition().duration(500).attr("r", radiusScale(.05));
-        d3.select("#origin").html("Origin: " + d.properties.ADDRESS); 		
+        d3.select("#origin").html("Station: " + d.properties.ADDRESS); 		
         d3.select(this).attr("class", "stationNode sn_selected").style("fill", "#FFCB00").transition().duration(500).attr("r", radiusScale(.1));
         showRoutes(stationSelected);
       }
     });
-    sfeature.on("mouseover",function(d){
-      if (stationSelected > 0) {
-        d3.select("#destination").html("Destination: " + d.properties.ADDRESS);
-      } else { 
-        d3.select("#origin").html("Origin: " + d.properties.ADDRESS);        	
-      }
+    sfeature.on("mouseover", function(d){
       d3.select(this).style("cursor", "pointer")
-      .transition().duration(100).style("stroke-width", "1.5px");
+        .transition().duration(100).style("stroke-width", "1.5px");
       add_map_label(d);
+      if (stationSelected == 0) { 
+        d3.select("#origin").html("Station: " + d.properties.ADDRESS);        	
+      }
     }); 
     sfeature.on("mouseout",function(d){
       d3.select(this).transition().duration(100).style("stroke-width", ".5px");
@@ -132,11 +134,12 @@ function showRoutes(from_station, to_station) {
 }
 
 function drawGraph(gData) {
+  d3.selectAll("#graph_header").html("Top 20 Destinations ").append("em").html("(total trips in 2012)");
   var bars = gBars.selectAll("rect")
     .data(gData)
     .enter()
-    .append("rect")
-    .attr("x", 3)
+    .append("rect")      
+    .attr("x", 5)
     .attr("y", function(d, i){ return i * (h_graph / gData.length) + 2; })
     .attr("width", 0)
     .attr("height", h_graph / 20 - 2)
@@ -146,7 +149,7 @@ function drawGraph(gData) {
       //checks to see if the graph is longer than the svg width and draws another rect on top to show the total length
       if (d[1]/2 > w_graph-3){
         gExtraBars.append("rect")
-          .attr("x", 3)
+          .attr("x", 5)
           .attr("y", i * (h_graph / gData.length) + 3)
           .attr("width", 0)
           .attr("height", h_graph / 20 - 4)
@@ -160,7 +163,7 @@ function drawGraph(gData) {
   bars.on("mouseover",function(d){
     d3.select(this).style("cursor", "pointer");
     d3.select(this).attr("filter","url(#f1)").style("stroke-width", "1px").style("stroke", "#929292");
-    d3.select("#destination").html("Destination: " + stationLUT[d[0]]);
+    ///d3.select("#destination").html("Destination: " + stationLUT[d[0]]);
     var s = d3.select("#stations").selectAll("circle").filter(function(sd) {return sd.properties.TERMINAL_N == d[0];});
     s.transition().duration(500).delay(500).each(c_flash);
     add_map_label(s.data()[0]);
@@ -171,16 +174,19 @@ function drawGraph(gData) {
       .transition().duration(500).attr("r", radiusScale(0.05));
     remove_map_label();
   });
+  bars.on("mousedown",function(d){
+    showRoutes(stationSelected, d[0]); 
+  });
   
   //add labels
   var text = gText.selectAll("text")
     .data(gData)
     .enter()
     .append("text")
-    .attr("x", 5)
+    .attr("x", 7)
     .attr("y", function(d, i){ return i * (h_graph / gData.length) + (h_graph / 20 - 4)})
     .style("font-family", "sans-serif")
-    .style("font-size", "9pt")
+    .style("font-size", "10pt")
     .style("font-weight", "bold")
     .style("fill", "#FFCB00")
     .attr("filter", "url(#f2)")
@@ -192,7 +198,7 @@ function drawGraph(gData) {
     d3.select(this).style("cursor", "pointer");
     d3.select("#graphbars").selectAll("rect").filter(function(bd) {return bd[0] == d[0];})
       .attr("filter","url(#f1)").style("stroke-width", "1px").style("stroke", "#929292");
-    d3.select("#destination").html("Destination: " + stationLUT[d[0]]);
+    //d3.select("#destination").html("Destination: " + stationLUT[d[0]]);
     var s = d3.select("#stations").selectAll("circle").filter(function(sd) {return sd.properties.TERMINAL_N == d[0];});
     s.transition().duration(500).delay(500).each(c_flash);
     add_map_label(s.data()[0]);
@@ -203,6 +209,9 @@ function drawGraph(gData) {
     d3.select("#stations").selectAll("circle").filter(function(sd) {return sd.properties.TERMINAL_N == d[0];})
       .transition().duration(500).attr("r", radiusScale(0.05));
     remove_map_label();
+  });
+  text.on("mousedown",function(d){
+    showRoutes(stationSelected, d[0]); 
   });
 }
 //converts screen coords to latlng for leaflet
